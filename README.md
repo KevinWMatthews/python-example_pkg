f989f36fabbd747e49c969968b4dcc7e5b21c162
Kevin Matthews <kmatthews@cyberdata.net>
2018-08-14 17:17:48 -0700

    Create setup.py with license and readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# Example Package
+
+A sample Python package.
+Created following the tutorial at [packaging.python.org](https://packaging.python.org/tutorials/packaging-projects/).
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example_pkg",
+    version="0.0.1",
+    author="Kevin Matthews",
+    author_email="please_dont_email_me@not_mail.com",
+    description="An example of a python package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/KevinWMatthews/python-example_pkg",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    )
+)
