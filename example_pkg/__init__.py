d010578e07dad8fc19b48b968ae33d665e861429
Kevin Matthews <kmatthews@cyberdata.net>
2018-08-16 19:08:09 -0700

    Add an example function in a module

--- example_pkg/__init__.py
+++ example_pkg/__init__.py
@@ -1,4 +1,10 @@
 name = "example_pkg"
 
+# This allows you to directly call: example_pkg.example_function2()
+from .example_module import example_function2
+
+# This requires you to call: example_pkg.example_module.example_function2()
+# from . import example_module
+
 def example_function():
     print("Hello! This is an example function in __init__.py")
