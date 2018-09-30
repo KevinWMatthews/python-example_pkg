8a56c3e76576dd649b69166ee9640156a64bbe7c
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-09-30 15:30:00 -0700

    Add syntax example for python unittest library

--- example_pkg/tests/test_example.py
+++ example_pkg/tests/test_example.py
@@ -17,3 +17,22 @@ class TestExampleModule2(TestCase):
     def test_is_string(self):
         s = example_module2.function_from_example_module2()
         self.assertTrue(isinstance(s, str))
+
+class TestSyntaxExample(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    @unittest.skip('Must put message here')
+    def test_will_be_skipped(self):
+        pass
